ab5895196148407a1e64500cded2786b97af3a57
rmaOrt <roma_628@hotmail.com>
2015-09-16 02:09:03 -0500

    Cargue inicial proyecto ArDesign

---
+++ app/src/main/java/com/rom/ar/ardesign/utils/enumerator/ModelParams.java
@@ -0,0 +1,42 @@
+package com.rom.ar.ardesign.utils.enumerator;
+
+public enum ModelParams {
+
+    PARED_LADRILLO(1L, "ARSources/DesignModels/VizAidModel/silueta.obj", "ARSources/DesignModels/muroLadri.obj", "ARSources/TrackingConfig/Tracking.xml");
+
+    private final Long id;
+    private final String strPerfilPath;
+    private final String strModelPath;
+    private final String strTrackingConfig;
+
+    ModelParams(Long id, String strPerfilPath, String strModelPath, String strTrackingConfig) {
+        this.id = id;
+        this.strPerfilPath = strPerfilPath;
+        this.strModelPath = strModelPath;
+        this.strTrackingConfig = strTrackingConfig;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStrPerfilPath() {
+        return strPerfilPath;
+    }
+
+    public String getStrModelPath() {
+        return strModelPath;
+    }
+
+    public String getStrTrackingConfig() {
+        return strTrackingConfig;
+    }
+
+    public static ModelParams fromId(final Long id) {
+        for (ModelParams m : ModelParams.values()) {
+            if (m.id.equals(id))
+                return m;
+        }
+        return null;
+    }
+}
